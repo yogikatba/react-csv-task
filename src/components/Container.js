e3326fcb7288d93da1511bc9d3daa1c75ac7a631
yogikatba <yogikatba16@gmail.com>
2020-06-30 20:45:21 +0530

    Implement displaying order

---
+++ src/components/Container.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Container() {
+  return (
+    <div class="container">
+      <hr />
+      <div class="row">
+        <div class="col-sm">Order Id</div>
+        <div class="col-sm">Cust Id</div>
+        <div class="col-sm">Pin Code</div>
+        <div class="col-sm">Order Date</div>
+        <div class="col-sm">Items</div>
+      </div>
+      <hr />
+    </div>
+  );
+}
--- src/components/DisplayOrder.js
+++ src/components/DisplayOrder.js
@@ -1,24 +1,29 @@
-import React from 'react'
+import React from "react";
 
-export default function DisplayOrder({object}) {
-
-    let {items} = object
-    let item = []
-    let j=''
-    for(let i=0;i<items.length;i++){
-        while(items[i]!==';'){
-            j+=items[i]
-            i++;
-        }
-        item.push(j)
-        console.log(item)
-        j=''
+export default function DisplayOrder({ object }) {
+  let { items } = object;
+  let item = [];
+  let j = "";
+  for (let i = 0; i < items.length; i++) {
+    while (items[i] !== ";") {
+      j += items[i];
+      i++;
     }
-    // {item.map(i=><div>{i}</div>)}
+    item.push(j);
+    j = "";
+  }
+  // {item.map(i=><div>{i}</div>)}
 
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">{object.orderId}</div>
+        <div class="col-sm">{object.customerId}</div>
+        <div class="col-sm">{object.pin}</div>
+        <div class="col-sm">{object.date}</div>
+        <div class="col-sm">{item.map((i, idx)=><div key={idx}>{i}</div>)}</div>
+      </div>
+      <hr/>
+    </div>
+  );
 }
